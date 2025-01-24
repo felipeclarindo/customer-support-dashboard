773b84d819953aff7e38e9d98a2883bcc3cf305f
felipeclarindo <fglpc2@gmail.com>
2025-01-24 16:54:55 -0300

    docs(docstring): Adding docstrings to functions and classes for better code reading.

--- src/modules/generate_insights.py
+++ src/modules/generate_insights.py
@@ -2,7 +2,17 @@ import google.generativeai as genai
 from pandas import DataFrame
 import os
 
-def generate_insights(data: DataFrame):
+
+def generate_insights(data: DataFrame) -> str:
+    """
+    Generate insights from a dataset using GPT-3.
+
+    Args:
+        data (DataFrame): The dataset to generate insights from.
+
+    Returns:
+        str: Returns the generated insights.
+    """
     api_key = os.getenv("GEMINI_API_KEY")
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel("gemini-1.5-flash")
@@ -10,4 +20,3 @@ def generate_insights(data: DataFrame):
     prompt = f"O dataset a seguir corresponse a metricas de negócio de uma operação de suporte. Me informe 5 insights sobre este dataset. {data}"
     response = model.generate_content(prompt)
     return response
-        
